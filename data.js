933703113bac1b2c15ff2fcbefec40bef8541fc1
rishi <sidharthmangaraj61@gmail.com>
2026-05-02 11:41:10 +0530

    adding products

--- data.js
+++ data.js
@@ -15,6 +15,14 @@ const paintings = [
     image: "images/kidintear.jpeg",
     category: "Charcoal Art"
   },
+   {
+     id: 6,
+    title: "charcoal eyes",
+    description: "charcoal art of an eye.",
+    price: 1500,
+    image: "images/charcoal eyes.jpeg",
+    category: "Charcoal Art"
+  },
   {
     id: 3,
     title: "Golden Hour",
@@ -23,5 +31,41 @@ const paintings = [
     image: "images/golden.jpeg",
     category: "Paintings"
   },
+  {
+     id: 4,
+    title: "a girl in shining",
+    description: "pen sketch of a girl.",
+    price: 3500,
+    image: "images/a girl in shining.jpeg",
+    category: "Pen sketches"
+  },
+   
+  {
+     id: 5,
+    title: "pen eyes",
+    description: "pen sketch of an eye.",
+    price: 1500,
+    image: "images/pen eyes.jpeg",
+    category: "Pen sketches"
+  },
+  {
+     id: 8,
+    title: "baby steps",
+    description: "dance of baby feet.",
+    price: 3500,
+    image: "images/baby steps.jpeg",
+    category: "Pen sketches"
+  },
+ {  
+     id: 7,
+    title: "alway an eye",
+    description: "charcoal art of a girl with scarf.",
+    price: 5500,
+    image: "images/always an eye.jpeg",
+    category: "Charcoal Art"
+  },
+ 
+  
+  
   // add more paintings here...
 ];
